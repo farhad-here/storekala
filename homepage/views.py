e8ee67a53787491fe76c7949bf8f6cd8f1c556bb
Farhad_Ghaherdoost <farhadghaher@gmail.com>
2026-06-20 01:33:34 +0330

    Initiate Homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
 from products.models import Product
+from django.views.generic import ListView
+
+class HomePageView(ListView):
+    model = Product
+    template_name = 'home/home.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.select_related('store').order_by('-created_at')
