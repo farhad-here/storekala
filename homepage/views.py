784b7cb1cd4411489abc86cdb733f27dc2b1c3c9
Farhad_Ghaherdoost <farhadghaher@gmail.com>
2026-06-20 02:03:33 +0330

    Fix a homepage views.py

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from products.models import Product
+from stores.models import Product
 from django.views.generic import ListView
 
 class HomePageView(ListView):
