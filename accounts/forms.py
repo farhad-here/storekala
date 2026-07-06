9058ce97a12ce02e56438f4ad98dc7b2fc93ddab
Farhad_Ghaherdoost <farhadghaher@gmail.com>
2026-07-06 23:40:27 +0330

    Fix bug

--- accounts/forms.py
+++ accounts/forms.py
@@ -74,11 +74,7 @@ class CustomAuthenticationForm(AuthenticationForm):
         password = self.cleaned_data.get('password')
 
         if phone_number is not None and password:
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> testing
             self.user_cache = authenticate(
                 self.request, 
                 username=phone_number, 
